78c48ecaecb53a106a1d08326a94510b54858e2c
Emerson Mascarenhas <esmascarenhas12@gmail.com>
2021-11-18 10:39:34 -0300

    Projeto Dio APIRest para calculo de distância entre cidades

---
+++ src/main/java/br/com/esm/findyCity/FindyCityApplication.java
@@ -0,0 +1,13 @@
+package br.com.esm.findyCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/esm/findyCity/FindyCityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.esm.findyCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
